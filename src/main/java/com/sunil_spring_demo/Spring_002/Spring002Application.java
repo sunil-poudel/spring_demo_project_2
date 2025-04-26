f9464e2c3440550b4f73203b50d25bd968bc09ad
Sunil Poudel <suneelpdl90@gmail.com>
2025-04-26 22:28:51 +0545

    initial commit

---
+++ src/main/java/com/sunil_spring_demo/Spring_002/Spring002Application.java
@@ -0,0 +1,13 @@
+package com.sunil_spring_demo.Spring_002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring002Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunil_spring_demo/Spring_002/Spring002ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunil_spring_demo.Spring_002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring002ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
