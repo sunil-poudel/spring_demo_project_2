901cd86e56f587e7b5c4a264bf9c87a31731e556
Sunil Poudel <suneelpdl90@gmail.com>
2025-04-27 17:14:05 +0545

    hide banner and logs in console

--- src/main/java/com/sunil_spring_demo/Spring_002/Spring002Application.java
+++ src/main/java/com/sunil_spring_demo/Spring_002/Spring002Application.java
@@ -1,7 +1,9 @@
 package com.sunil_spring_demo.Spring_002;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Spring002Application {
@@ -10,4 +12,10 @@ public class Spring002Application {
 		SpringApplication.run(Spring002Application.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(){
+		return runner -> {
+			System.out.println("Hello!");
+		};
+	}
 }
