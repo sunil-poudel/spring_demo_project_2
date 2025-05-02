3ba205fcb0bb60380281f1dede08a442b1dc022d
Sunil Poudel <suneelpdl90@gmail.com>
2025-05-02 16:21:08 +0545

    JPA ANNOTATIONS

---
+++ src/main/java/com/sunil_spring_demo/Spring_002/entity/Student.java
@@ -0,0 +1,82 @@
+package com.sunil_spring_demo.Spring_002.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="student")
+public class Student {
+    //define fields
+    @Id
+    @Column(name="id")
+    private int id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+
+    //define constructors
+    public Student(){
+
+    }
+
+    public Student(String firstName, String lastName, String email){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.email=email;
+    }
+
+    //define getters/setters
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    //define toString
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
